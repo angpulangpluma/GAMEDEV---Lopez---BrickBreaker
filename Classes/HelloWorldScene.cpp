6ec5135bbc29a784273c6b537d58d7467b8a454d
angpulangpluma <mariennelopez30@gmail.com>
2016-02-12 11:40:12 +0800

    square one
    just the simplest "hello world" code in cocos2d-x in cpp (C++)

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,36 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+	auto label = Label::createWithSystemFont("Hello World", "Arial", 96);
+	label->setAnchorPoint(Vec2(0, 0));
+	label->setPosition(50, 350);
+	this->addChild(label, 1);
+    
+    return true;
+}
