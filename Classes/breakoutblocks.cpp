99aeb310301d001c307313c5522e377c9eb64d9e
angpulangpluma <mariennelopez30@gmail.com>
2016-02-21 15:41:19 +0800

    coding stuff part 1 of ?
    - basic motion of paddle moving left and right w/ ball
    - changed ball sprite
    - changed wall sprite (if it was just blue, one cannot distinguish one
    wall from the other???)

--- Classes/breakoutblocks.cpp
+++ Classes/breakoutblocks.cpp
@@ -5,7 +5,7 @@ USING_NS_CC;
 Scene* Breakout::createScene()
 {
     // 'scene' is an autorelease object
-    auto scene = Scene::createWithPhysics();
+    auto scene = Scene::create();
     
     // 'layer' is an autorelease object
     auto layer = Breakout::create();
@@ -28,30 +28,95 @@ bool Breakout::init()
     }
     
 	walls = Vector<Sprite*>{};
+	start = false;
 
 	Sprite* one = Sprite::create("wall.png");
 	one->setAnchorPoint(Vec2(0, 0));
 	one->setPosition(0, 250);
-	this->addChild(one, 1);
+	this->addChild(one, 0);
 	walls.pushBack(one);
 	auto posx = one->getPositionX();
 	for (int i = 1; i < 1080/40; i++){
 		Sprite* w = Sprite::create("wall.png");
 		w->setAnchorPoint(Vec2(0, 0));
 		w->setPosition(posx + 40, one->getPositionY());
-		this->addChild(w, 1);
+		this->addChild(w, 0);
 		posx = w->getPositionX();
 		walls.pushBack(w);
 	}
 
 	platform = Sprite::create("platform.png");
-	platform->setAnchorPoint(Vec2(0, 0));
-	platform->setPosition(ball->getPosition());
-	this->addChild(platform, 1);
 	ball = Sprite::create("ball.png");
 	ball->setAnchorPoint(Vec2(0, 0));
-	ball->setPosition(platform->getPositionX(),platform->getPositionY()+50);
-	this->addChild(ball, 1);
+	platform->setPosition(this->getBoundingBox().getMidX()-30, this->getBoundingBox().getMidY() - 150);
+	ball->setPosition(this->getBoundingBox().getMidX(), platform->getPositionY() + 25);
+	platform->setAnchorPoint(Vec2(0, 0));
+	this->addChild(platform, 0);
+	this->addChild(ball, 0);
 
+	auto eventListener = EventListenerKeyboard::create();
+	eventListener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode,
+		Event* event){
+
+		if (keys.find(keyCode) == keys.end()){
+			keys[keyCode] = std::chrono::high_resolution_clock::now();
+		}
+
+		Vec2 loc = platform->getPosition();
+		switch (keyCode){
+		case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+		case EventKeyboard::KeyCode::KEY_A:{ //left
+			platform->setPosition(loc.x-5, loc.y);
+			log("left");
+		};  break;
+		case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+		case EventKeyboard::KeyCode::KEY_D:{ //right
+			platform->setPosition(loc.x+5, loc.y);
+			log("right");
+		}; break;
+		}
+	};
+
+	eventListener->onKeyReleased = [=](EventKeyboard::KeyCode keyCode, Event* event){
+		keys.erase(keyCode);
+	};
+
+	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener, platform);
+
+	this->scheduleUpdate();
     return true;
 }
+
+void Breakout::update(float delta){
+
+	Vec2 ploc = platform->getPosition();
+	Vec2 bloc = ball->getPosition();
+	if (isKeyPressed(EventKeyboard::KeyCode::KEY_SPACE) && !start){
+		start = true;
+		log("start!");
+	}
+	if (isKeyPressed(EventKeyboard::KeyCode::KEY_LEFT_ARROW) ||
+		isKeyPressed(EventKeyboard::KeyCode::KEY_A)){
+		log("left!");
+		if (!start)
+			ball->setPosition(ploc.x+30, bloc.y);
+		platform->setPosition(ploc.x-5, ploc.y);
+	}
+	else if (isKeyPressed(EventKeyboard::KeyCode::KEY_RIGHT_ARROW) ||
+		isKeyPressed(EventKeyboard::KeyCode::KEY_D)){
+		log("right!");
+		if (!start)
+			ball->setPosition(ploc.x+35, bloc.y);
+		platform->setPosition(ploc.x+5, ploc.y);
+	}
+}
+
+bool Breakout::isKeyPressed(EventKeyboard::KeyCode code){
+	if (keys.find(code) != keys.end()){
+		return true;
+	}
+	return false;
+}
+
+std::map<cocos2d::EventKeyboard::KeyCode,
+	std::chrono::high_resolution_clock::time_point> Breakout::keys;
--- Classes/breakoutblocks.h
+++ Classes/breakoutblocks.h
@@ -4,13 +4,19 @@
 
 class Breakout : public cocos2d::Layer
 {
+private:
+	static std::map<cocos2d::EventKeyboard::KeyCode,
+		std::chrono::high_resolution_clock::time_point> keys;
 public:
 	cocos2d::Sprite* ball;
 	cocos2d::Sprite* platform;
 	cocos2d::Vector<cocos2d::Sprite*> walls;
     static cocos2d::Scene* createScene();
+	bool start;
 
     virtual bool init();
+	virtual void update(float) override;
+	bool isKeyPressed(cocos2d::EventKeyboard::KeyCode);
     
     // implement the "static create()" method manually
     CREATE_FUNC(Breakout);
